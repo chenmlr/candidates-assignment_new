bab91ed505d5de13df97f535fb9455522c05b31d
Chen M <chen.meler94@gmail.com>
2021-07-29 01:35:39 +0300

    final version

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+
+sudo -u vagrant touch /home/vagrant/.ssh/config
+#sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+echo -e "Host server2\n    HostName 192.168.100.11" >> /home/vagrant/.ssh/config
+#sudo systemctl restart sshd
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo apt-get update
+sudo apt-get install sshpass
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+sudo -u vagrant ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ""
+sudo -u vagrant touch /home/vagrant/.ssh/known_hosts
+sudo -u vagrant ssh-keyscan -H 192.168.100.10 >> /home/vagrant/.ssh/known_hosts
+
+sudo -u vagrant sshpass -p "vagrant" ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.100.10
+
+sudo -u vagrant sshpass -p "vagrant" ssh vagrant@192.168.100.10 "/home/vagrant/exercise5-fix"
+
+sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
